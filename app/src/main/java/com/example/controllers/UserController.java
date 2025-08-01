e9a997f34ebcca90eb9e32aa1dbf46ea284611ec
Rajeswari <rajeswari6kaligatla@gmail.com>
2025-08-01 16:35:12 +0530

    connecting spring-boot , postgresql through docker

---
+++ app/src/main/java/com/example/controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.example.controllers;
+
+import com.example.entities.User;
+import com.example.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public List<User> getUsers() {
+
+        return service.getAllUsers();
+    }
+}
