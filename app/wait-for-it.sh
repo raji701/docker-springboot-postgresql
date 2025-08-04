919178bbe3dde40dcea6ce12e96daa5ac57bdb31
Rajeswari <rajeswari6kaligatla@gmail.com>
2025-08-04 16:35:07 +0530

    different-docker-network-springboot-postgresql-app

---
+++ app/wait-for-it.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# wait-for-it.sh
+
+host="$1"
+shift
+port="$1"
+shift
+
+cmd="$@"
+
+until nc -z "$host" "$port"; do
+  >&2 echo "Waiting for $host:$port to be available..."
+  sleep 1
+done
+
+>&2 echo "$host:$port is up - executing command"
+exec $cmd
